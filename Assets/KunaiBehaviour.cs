f3a02aa24fd6aa4382c34eb90475e8e2c44eb870
Bonnie Jenkins <18016a@totton.ac.uk>
2023-04-17 12:28:23 +0100

    Witch Attacks
    Kunai now spawns and fly forwards. Need to make them one at a time and spawn in three different positions

---
+++ Assets/KunaiBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiBehaviour : MonoBehaviour
+{
+    Vector3 target;
+    bool flying;
+    public float speed;
+    public int damage;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").GetComponent<Transform>().position;
+    }
+
+    private void Update()
+    {
+        target.y += 50;
+        transform.LookAt(target);
+
+        if (flying)
+        {
+            Vector3 targetPos = target;
+            targetPos.y = transform.position.y;
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+    }
+
+    [ContextMenu("Shoot")]
+    public void Shoot()
+    {
+        flying = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        other.transform.GetComponent<Health>().DealDamage(damage, false);
+        Destroy(gameObject);
+    }
+}
