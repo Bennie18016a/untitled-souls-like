0600282344dd9acf5ac5c6c414d45d4c85c5255a
Bonnie Jenkins <18016a@totton.ac.uk>
2023-05-10 11:54:50 +0100

    URP and Basic Lighting

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public float minIntensity = 2.0f;
+    public float maxIntensity = 3.0f;
+    public float Timer = 0.1f;
+    private new Light light = null;
+
+    private void Start()
+    {
+        light = this.GetComponentInChildren<Light>();
+        InvokeRepeating("Flicker", Timer, Timer);
+    }
+    private void Flicker()
+    {
+        float R = Random.Range(minIntensity, maxIntensity);
+        light.intensity = R;
+    }
+}
